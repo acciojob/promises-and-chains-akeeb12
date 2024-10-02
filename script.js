f64f871ade338d8582e05f0421e8c89a77547130
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-10-02 14:32:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+document.getElementById('myform').addEventListener('submit', function (event) {
+event.preventDefault();
+
+	const name = document.getElementById('name').value;
+	const age = document.getElementById('age').value;
+
+	if(!name || !age){
+		alert("Please fill out all the fileds.!");
+		return;
+	}
+
+	new Promise ((resolv, reject) =>{
+		setTimeout() =>{
+			if(age >= 18){
+				resolve (`Welcome, ${name} You can vote.`);
+			}else{
+				reject (`Oh Sorry ${name} you're not old enough to vote.`);
+			}
+		}, 4000);
+	});
+
+myPromise.then(function (message) {
+	alert(message);
+}).catch(function (message) {
+	alert(message);
+});
+});
+
+
+
+
