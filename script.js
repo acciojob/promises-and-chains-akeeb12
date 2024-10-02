096b4686c1465c5f3a06a182cbfe9c16abd6440f
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-10-02 18:49:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
-//your JS code here. If required.
-document.getElementById('myform').addEventListener('submit', function (event) {
-event.preventDefault();
 
-	const name = document.getElementById('name').value;
-	const age = document.getElementById('age').value;
+document.getElementById("userForm").addEventListener("submit", function(event) {
+  event.preventDefault(); 
+  const age = document.getElementById("age").value;
+  const name = document.getElementById("name").value;
 
-	if(!name || !age){
-		alert("Please fill out all the fileds.!");
-		return;
-	}
+  if (!age || !name) {
+    alert("Please fill out both fields.");
+    return;
+  }
 
-	new Promise ((resolve, reject) =>{
-		setTimeout() =>{
-			if(age >= 18){
-				resolve (`Welcome, ${name} You can vote.`);
-			}else{
-				reject (`Oh Sorry ${name} you're not old enough to vote.`);
-			}
-		}, 4000);
-	});
 
-myPromise.then(function (message) {
-	alert(message);
-}).catch(function (message) {
-	alert(message);
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, 4000);
+  })
+  .then(() => {
+    alert(`Welcome, ${name}. You can vote.`);
+  })
+  .catch(() => {
+    alert(`Oh sorry ${name}. You aren't old enough.`);
+  });
 });
-});
-
-
-
-
